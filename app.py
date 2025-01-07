2c44eb22e55063ed56339268a0282fd3e1506b88
KKonuru <kausthubhkonuru@gmail.com>
2025-01-07 18:35:06 -0500

    Implemented a slider to adjust the llm temperature

--- app.py
+++ app.py
@@ -3,21 +3,13 @@ import os
 from Agent import CodingAgent
 import subprocess
 
-#Pages title
-st.title('Code Chat')
-st.write('Welcome to Code Chat! This is a simple chatbot that can help you with your coding questions. Provide a link to your code repository and ask your question. The chatbot will answer your questions using access to the codebase.')
 
-# Sidebar to create new chats
-st.sidebar.title("Repository Information")
+#On change function that adjusts the temperature when the slider is changed
+def tempChange():
+    if "chatbot" in st.session_state:
+        st.session_state.chatbot.setAgentTemp(st.session_state.agent_temp)
+        st.session_state.chatbot.setCodeLLMTemp(st.session_state.code_llm_temp)
 
-# Folder inputs
-folder_input = st.text_input("Enter folder path", "")
-
-#Check if the user input folder is a valid path and is a directory
-def validateFolderPath(folder_path):
-    if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
-        return False
-    return True
 
 #Lists all the file names that are stored in the vector database
 #Some files are exlcuded based on the file type
@@ -42,6 +34,38 @@ def openInVscode():
     else:
         st.error("Please provide a valid folder path.")
 
+#Check if the user input folder is a valid path and is a directory
+def validateFolderPath(folder_path):
+    if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
+        return False
+    return True
+
+#Pages title
+st.title('Code Chat')
+st.write('Welcome to Code Chat! This is a simple chatbot that can help you with your coding questions. Provide a link to your code repository and ask your question. The chatbot will answer your questions using access to the codebase.')
+
+on = st.toggle("Activate Chatbot Agent Mode")
+if on:
+    st.write("Chatbot Agent Mode is ON. You can ask questions about the codebase. You can also ask programming related questions and get youtube links to helpful videos.")
+else:
+    st.write("Chatbot Agent Mode is OFF. You are interacting with the RAG application. You can ask questions about the codebase and general programming questions.")
+# Sidebar to create new chats
+st.sidebar.title("About Code Chat")
+st.sidebar.write("You can interact with either the multi-tool agent or the RAG application. The rag application can answer questions on the codebase and general programming questions. The multi-tool agent can do the functions of the rag application and also answer questions on programming and provide youtube links and wikipedia summaries.")
+st.sidebar.write("Check out the repository for more information on the project [here](https://github.com/KKonuru/Code-Chat).")
+st.sidebar.title("Settings")
+st.sidebar.write("You can change the settings of the chatbot here.")
+
+agent_temp = st.sidebar.slider("Agent LLM Temperature", 0.0, 1.0, 0.05,on_change=tempChange)
+st.session_state.agent_temp = agent_temp
+code_llm_temp = st.sidebar.slider("Code LLM Temperature", 0.0, 1.0, 0.85,on_change=tempChange)
+st.session_state.code_llm_temp = code_llm_temp
+st.sidebar.title("Repository Information")
+
+# Folder inputs
+folder_input = st.text_input("Enter folder path", "")
+
+
 #Save the folder path to check if it has been changed
 if "folder_path" not in st.session_state:
     st.session_state.folder_path = None
@@ -51,7 +75,7 @@ if folder_input and validateFolderPath(folder_input) and st.session_state.folder
     st.session_state.folder_path = folder_input
     st.sidebar.write(f"Selected folder path: {st.session_state.folder_path}")
     with st.spinner('Loading Codebase...'):
-            chatbot = CodingAgent(st.session_state.folder_path)             
+            chatbot = CodingAgent(st.session_state.folder_path,st.session_state.agent_temp,st.session_state.code_llm_temp)             
     st.session_state.chatbot = chatbot
     st.sidebar.button("Open in VSCode",on_click=openInVscode)
     displayFiles()
@@ -74,6 +98,12 @@ def responseGenerator(input_text):
         response = "The chatbot is not initialized yet. Please select a folder path."
     return response
 
+def ragResponse(input_text):
+    if "chatbot" in st.session_state:
+        response = st.session_state.chatbot.getRagApp().llmResponse(input_text)
+    else:
+        response = "The chatbot is not initialized yet. Please select a folder path."
+    return response
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -89,7 +119,11 @@ if prompt := st.chat_input("Ask away!"):
 
     # Generate and display the assistant's response
     with st.chat_message("assistant"):
-        response = responseGenerator(prompt)  # Get the response
-        st.markdown(response)  # Display the response
+        if on:
+            response = responseGenerator(prompt)  # Get the response
+            st.markdown(response)
+        else: 
+            response = ragResponse(prompt)
+            response = st.write_stream(response)
         st.session_state.messages.append({"role": "assistant", "content": response})  # Save it
 
