292c5a0ba9d9c9516b8a8861d2a193257889aa84
KKonuru <kausthubhkonuru@gmail.com>
2025-01-06 20:42:47 -0500

    Refactored application

--- app.py
+++ app.py
@@ -2,20 +2,25 @@ import streamlit as st
 import os
 from main import codeChat
 import subprocess
-# Page title and instructions
+
+#Pages title
 st.title('Code Chat')
-st.write('Welcome to Code Chat! This is a simple chatbot that can help you with your coding questions. Provide a link to your code repository and ask your question. The chatbot will try to help you with your query.')
+st.write('Welcome to Code Chat! This is a simple chatbot that can help you with your coding questions. Provide a link to your code repository and ask your question. The chatbot will answer your questions using access to the codebase.')
 
 # Sidebar to create new chats
 st.sidebar.title("Repository Information")
 
-# Folder selection
+# Folder inputs
 folder_input = st.text_input("Enter folder path", "")
 
-def validate_folder_path(folder_path):
+#Check if the user input folder is a valid path and is a directory
+def validateFolderPath(folder_path):
     if not os.path.exists(folder_path) or not os.path.isdir(folder_path):
         return False
     return True
+
+#Lists all the file names that are stored in the vector database
+#Some files are exlcuded based on the file type
 def displayFiles():
     fileNames = st.session_state.chatbot.getFileNames()
     st.sidebar.write("Files in the directory:")
@@ -23,7 +28,8 @@ def displayFiles():
     for file in fileNames:
         st.sidebar.write(file)
 
-def open_in_vscode():
+#Function for button on click 
+def openInVscode():
     if st.session_state.folder_path:
         # Run the VSCode command using subprocess
         try:
@@ -36,30 +42,31 @@ def open_in_vscode():
     else:
         st.error("Please provide a valid folder path.")
 
+#Save the folder path to check if it has been changed
 if "folder_path" not in st.session_state:
     st.session_state.folder_path = None
+
 # Save the folder path in a variable if provided
-if folder_input and validate_folder_path(folder_input) and st.session_state.folder_path != folder_input:
+if folder_input and validateFolderPath(folder_input) and st.session_state.folder_path != folder_input:
     st.session_state.folder_path = folder_input
     st.sidebar.write(f"Selected folder path: {st.session_state.folder_path}")
     with st.spinner('Loading Codebase...'):
             chatbot = codeChat(folder_input)
     st.session_state.chatbot = chatbot
-    st.sidebar.button("Open in VSCode",on_click=open_in_vscode)
+    st.sidebar.button("Open in VSCode",on_click=openInVscode)
     displayFiles()
-elif folder_input and validate_folder_path(folder_input):
+elif folder_input and validateFolderPath(folder_input):
     st.sidebar.write(f"Selected folder path: {st.session_state.folder_path}")
-    st.sidebar.button("Open in VSCode",on_click=open_in_vscode)
+    st.sidebar.button("Open in VSCode",on_click=openInVscode)
     displayFiles()
-elif folder_input and not validate_folder_path(folder_input):
+elif folder_input and not validateFolderPath(folder_input):
     st.sidebar.write("Invalid folder path. Please enter a valid folder path.")
     st.session_state.chatbot = None
 else:
     st.sidebar.write("No folder selected yet.")
 
-
-
-def response_generator(input_text):
+#Returns the streamed response from the chatbot
+def responseGenerator(input_text):
     #Only if st.session_state.chatbot is defined
     if "chatbot" in st.session_state:
         response = st.session_state.chatbot.llmResponse(input_text)
@@ -81,5 +88,5 @@ if prompt := st.chat_input("Ask away!"):
 
     with st.chat_message("assistant"):
         # Here you can integrate your RAG system for generating responses
-        response = st.write_stream(response_generator(prompt))
+        response = st.write_stream(responseGenerator(prompt))
     st.session_state.messages.append({"role": "assistant", "content": response})
